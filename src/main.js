e1463c9cfad94a87a134451d1d38074e5549a2eb
Mat Sanders <mat@ustwo.us>
2016-02-18 15:33:04 -0500

    Use an array as a data source for the names of cafes to display.

--- src/main.js
+++ src/main.js
@@ -11,6 +11,10 @@ import CafeCard from './CafeCard.vue'
 Vue.component('cafe-card', CafeCard)
 
 // start vue, and tell it that it should work across the scope of the 'body' element
+// and pass in data that includes cafeNames, an array containgin the names of cafes to show
 new Vue({
-  el: 'body'
+  el: 'body',
+  data: {
+    cafeNames: ["Cafe Grumpy", "Gimmie! Coffee", "Underline", "Stumptown", "Third Rail"]
+  }
 })
